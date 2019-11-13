c2d072c372df4f271e6c769c6ab53f89d81f4e39
nikkiedd <dolot_nicole@yahoo.com>
2019-11-13 15:15:49 +0200

    Added endpoint for list of tags

---
+++ app/recipe/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from recipe import views
+
+
+router = DefaultRouter()    # automatically registers the appropriate URLs for all the actions in our viewset
+router.register('tags', views.TagViewSet)
+
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls))
+]
